a8f5953a46ac20c42cef149f3e600443e42a213e
khs <khs@123.com>
2024-05-27 21:03:18 +0800

    feat: AutoComplete异步功能实现

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,7 +1,15 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+import AutoComplete, { DataSourceType } from './autoComplete'
 
-import AutoComplete from './autoComplete'
+interface LakerPlayerProps {
+  value: string
+  number: number
+}
+interface GithubUserProps {
+  login: string
+  url: string
+  avatar_url: string
+}
 
 const meta: Meta<typeof AutoComplete> = {
   title: 'Components/AutoComplete',
@@ -9,34 +17,108 @@ const meta: Meta<typeof AutoComplete> = {
   component: AutoComplete,
   parameters: {
     layout: 'centered',
+    source: {
+      type: 'code',
+    },
   },
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const DefaultAutoCompletes: Story = () => {
+export const ASimpleComplete: Story = (args = {}) => {
   const lakers = [
-    'khs',
-    'popr',
+    'bradley',
+    'pope',
+    'caruso',
     'cook',
     'cousins',
+    'james',
     'AD',
     'green',
     'howard',
     'kuzma',
-    'rando',
     'McGee',
-    'james',
-    'caruso',
+    'rando',
   ]
-
   const handleFetch = (query: string) => {
-    return lakers.filter((name) => name.includes(query))
+    return lakers
+      .filter((name) => name.includes(query))
+      .map((name) => ({ value: name }))
   }
-
   return (
-    <AutoComplete fetchSuggestions={handleFetch} onSelect={action('select')} />
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="输入湖人队球员英文名试试"
+    />
   )
 }
-DefaultAutoCompletes.storyName = '默认AutoComplete'
+ASimpleComplete.storyName = '1 基本的搜索'
+
+export const BCustomComplete = (args = {}) => {
+  const lakersWithNumber = [
+    { value: 'bradley', number: 11 },
+    { value: 'pope', number: 1 },
+    { value: 'caruso', number: 4 },
+    { value: 'cook', number: 2 },
+    { value: 'cousins', number: 15 },
+    { value: 'james', number: 23 },
+    { value: 'AD', number: 3 },
+    { value: 'green', number: 14 },
+    { value: 'howard', number: 39 },
+    { value: 'kuzma', number: 0 },
+  ]
+  const handleFetch = (query: string) => {
+    return lakersWithNumber.filter((player) => player.value.includes(query))
+  }
+  const renderOption = (item: DataSourceType) => {
+    const itemWithNumber = item as DataSourceType<LakerPlayerProps>
+    return (
+      <>
+        <b>名字: {itemWithNumber.value}</b>
+        <span>球衣号码: {itemWithNumber.number}</span>
+      </>
+    )
+  }
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="输入湖人队球员英文,自定义下拉模版"
+      renderOption={renderOption}
+    />
+  )
+}
+BCustomComplete.storyName = '2 自定义搜索结果模版'
+
+export const CAjaxComplete = (args = {}) => {
+  const handleFetch = (query: string) => {
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then((res) => res.json())
+      .then(({ items }) => {
+        return items
+          .slice(0, 10)
+          .map((item: any) => ({ value: item.login, ...item }))
+      })
+  }
+
+  const renderOption = (item: DataSourceType) => {
+    const itemWithGithub = item as DataSourceType<GithubUserProps>
+    return (
+      <>
+        <b>Name: {itemWithGithub.value}</b>
+        <span>url: {itemWithGithub.url}</span>
+      </>
+    )
+  }
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="输入 Github 用户名试试"
+      renderOption={renderOption}
+    />
+  )
+}
+CAjaxComplete.storyName = '3 支持异步搜索'
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,37 +1,69 @@
 import { ChangeEvent, useState } from 'react'
 import Input, { InputProps } from '../Input/input'
+import Icon from '../Icon/icon'
 
-export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[]
-  onSelect?: (item: string) => void
+// 来源数据的类型
+interface DataSourceObject {
+  value: string
+}
+export type DataSourceType<T = {}> = T & DataSourceObject
+export interface AutoCompleteProps
+  extends Omit<InputProps, 'onSelect' | 'onChange'> {
+  /**
+   * 返回输入建议的方法，可以拿到当前的输入，然后返回同步的数组或者是异步的 Promise
+   * type DataSourceType<T = {}> = T & DataSourceObject
+   */
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceType[] | Promise<DataSourceType[]>
+  /** 点击选中建议项时触发的回调*/
+  onSelect?: (item: DataSourceType) => void
+  /** 文本框发生改变的时候触发的事件*/
+  onChange?: (value: string) => void
+  /**支持自定义渲染下拉项，返回 ReactElement */
+  renderOption?: (item: DataSourceType) => React.ReactElement
 }
 
 export const AutoComplete = ({
   fetchSuggestions,
   onSelect,
   value,
+  renderOption,
   ...restProps
 }: AutoCompleteProps) => {
   const [inputValue, setInputValue] = useState(value)
-  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
     if (value) {
       const result = fetchSuggestions(value)
-      setSuggestions(result)
+      if (result instanceof Promise) {
+        setLoading(true)
+        result.then((data) => {
+          setLoading(false)
+          setSuggestions(data)
+        })
+      } else {
+        setSuggestions(result)
+      }
     } else {
       setSuggestions([])
     }
   }
 
-  const handleClick = (item: string) => {
-    setInputValue(item)
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
     setSuggestions([])
     if (onSelect) {
       onSelect(item)
     }
+  }
+
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value
   }
 
   const generateDropdown = () => {
@@ -39,8 +71,8 @@ export const AutoComplete = ({
       <ul>
         {suggestions.map((item, index) => {
           return (
-            <li key={index} onClick={() => handleClick(item)}>
-              {item}
+            <li key={index} onClick={() => handleSelect(item)}>
+              {renderTemplate(item)}
             </li>
           )
         })}
@@ -50,6 +82,12 @@ export const AutoComplete = ({
   return (
     <div className="rose-auto-complete">
       <Input value={inputValue} onChange={handleChange} {...restProps} />
+      {loading && (
+        <ul>
+          <Icon icon="spinner" spin />
+          ...loading
+        </ul>
+      )}
       {suggestions.length > 0 && generateDropdown()}
     </div>
   )
